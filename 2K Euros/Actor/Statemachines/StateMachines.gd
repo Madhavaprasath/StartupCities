90c9a96bca3268248bc564e75130ffc26501d04a
Madhava Prasath <Madhavprasath088@gmail.com>
2020-11-21 03:38:56 +0530

    Stateachines

---
+++ 2K Euros/Actor/Statemachines/StateMachines.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Statemachines
+
+var parent=get_parent()
+
+#states 
+var state={}
+var current_state 
+
+func _physics_process(delta):
+	state_logic(delta)
+	var state=transition(delta)
+	if state!=null:
+		animation(state)
+func state_logic(delta):
+	pass
+func transition(delta):
+	return null
+func animation(state):
+	pass
